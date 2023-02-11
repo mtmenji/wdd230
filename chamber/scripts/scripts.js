f7bed6359d771231dd7fb082d3e9c8d41112aead
mtmenji <mtmenji@gmail.com>
2023-02-10 20:08:38 -0700

    W06 Modules: Responsive Design

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -10,7 +10,6 @@ let year = date.getFullYear();
 document.querySelector('#date').innerHTML = day + ", " + dateNum + " " + month + " " + year;
 document.querySelector('#year').innerHTML = year;
 
-
 //Hamburger Menu
 function toggleMenu() {
     document.querySelector('#primaryNav').classList.toggle('open');
@@ -20,7 +19,6 @@ function toggleMenu() {
 const x = document.querySelector('#hamburgerBtn');
 x.onclick = toggleMenu;
 
-
 //Last modified date and time script.
 let currentDate = document.lastModified;
 document.querySelector('#updated').textContent = currentDate;
@@ -28,4 +26,16 @@ document.querySelector('#updated').textContent = currentDate;
 //Banner for Meeting
 if (day == "Monday" || day == "Tuesday") {
     document.querySelector("#meeting-banner").style.display = "block";
+}
+
+//Weather
+let degree = document.querySelector("#degree").textContent;
+console.log(degree);
+let speed = document.querySelector("#speed").textContent;
+console.log(speed);
+let chill = Math.round((35.74 + (0.6215 * degree))-(35.75 * Math.pow(speed,0.16)) + (0.4275*degree*Math.pow(speed,0.16)));
+console.log(chill);
+
+if (degree <= 50 && speed >= 3) {
+    document.querySelector("#chill").textContent = chill;
 }
