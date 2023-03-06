6e3e92f9310a4d9268198224a1475ca1988693cc
mtmenji <mtmenji@gmail.com>
2023-03-06 09:37:53 -0700

    W09 Modules

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,15 +1,24 @@
-const url = 'https://github.com/mtmenji/wdd230/blob/main/chamber/directory.json';
+const url = 'https://json.extendsclass.com/bin/a7d6c112a9a8';
+const cardButton = document.querySelector("#cards");
+const listButton = document.querySelector("#list");
 
 async function getBusinessData() {
     const response = await fetch(url);
     const data = await response.json();
     console.table(data.businesses);  // note that we reference the business array of the data object given thestructure of the json file
-    // displaybusinesses(data.businesses);
-    displayTable(data.businesses);
+    cardButton.addEventListener('click', () => {        
+        displayBusinesses(data.businesses);
+    });
+    listButton.addEventListener('click', () => {        
+        displayTable(data.businesses);
+    });
 }
 
 const displayBusinesses = (businesses) => {
-    const cards = document.querySelector('div.cards'); // select the output container element
+    document.querySelector(".cards").innerHTML = "";
+    document.querySelector(".cards").style.display = 'flex';
+    document.querySelector("table").innerHTML = "";
+    cards = document.querySelector('div.cards'); // select the output container element
 
     businesses.forEach((business) => {
       // Create elements to add to the div.cards element
@@ -18,20 +27,19 @@ const displayBusinesses = (businesses) => {
       let logo = document.createElement('img');
       let address = document.createElement('p');
       let phone = document.createElement('p');
-      let website = document.createElement('p');
+      let website = document.createElement('a');
   
       // Build the h2 content out to show the business's full name - finish the template string
       h2.textContent = business.name;
       address.textContent = business.address;
       phone.textContent = business.phone;
       website.textContent = business.website;
+      website.href = business.website;
   
       // Build the image logo by setting all the relevant attribute
       logo.setAttribute('src', business.image);
       logo.setAttribute('alt', `Business Logo`);
       logo.setAttribute('loading', 'lazy');
-    //   logo.setAttribute('width', '340');
-    //   logo.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(h2);
@@ -44,25 +52,37 @@ const displayBusinesses = (businesses) => {
   )} // end of function expression
 
 function displayTable(businesses) {
-  businesses.forEach((business) => {
+    document.querySelector(".cards").innerHTML = "";
+    document.querySelector(".cards").style.display = 'none';
+    document.querySelector("table").innerHTML = "";
+    const table = document.querySelector('table');
+
+    businesses.forEach((business) => {
     let tr = document.createElement('tr');
     let td_name = document.createElement('td');
     let td_address = document.createElement('td');
     let td_phone = document.createElement('td');
     let td_website = document.createElement('td');
+    let td_website_link = document.createElement('a');
 
     td_name.textContent = business.name;
     td_phone.textContent = business.phone;
     td_address.textContent = business.address;
-    td_website.textContent = business.website;
+    td_website_link.textContent = business.website;
+    td_website_link.href = business.website;
 
     tr.appendChild(td_name);
     tr.appendChild(td_phone);
     tr.appendChild(td_address);
-    tr.appendChild(td_website);
+    tr.appendChild(td_website_link);
 
-    document.querySelector('table').appendChild(tr);
+    table.appendChild(tr);
   })
 }
 
 getBusinessData();
+
+
+
+
+
